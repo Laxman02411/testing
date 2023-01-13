9f5087800a3639d93761af71c77b17b18f87e1f4
Laxman reddy <laxmanreddyazure@gmail.com>
2023-01-13 17:13:43 +0530

    dds1

--- main.tf
+++ main.tf
@@ -120,4 +120,18 @@ resource "aws_instance" "web-1" {
 	CostCenter = "ABCD"
     }
 }
-
+resource "aws_instance" "web-2" {
+    ami = var.imagename
+    availability_zone = "us-east-1a"
+    instance_type = "t2.micro"
+    key_name = "LaptopKey"
+    subnet_id = "${aws_subnet.subnet1-public.id}"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    tags = {
+        Name = "Dev-Server-2"
+        Env = "Dev"
+        Owner = "Sree"
+	CostCenter = "ABCD"
+    }
+}
